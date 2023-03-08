c77157b67bcaf507e1272c715d7eef056b1b69df
Daniel Noun <danielnoun21@gmail.se>
2023-03-08 11:56:39 +0100

    a

--- AffärsLager/Classes/Bokning.cs
+++ AffärsLager/Classes/Bokning.cs
@@ -16,9 +16,9 @@ namespace Business.Classes
         public Expidit ExpiditNamn { get; set; } = null!;
 
         public virtual IList<Bok> BokadeBöcker { get; set; } = new List<Bok>();
-        public DateTime? StartLån { get; set; }  // Det datum som skrivs in när man först planerar att boka boken ifrån i menyval 1
-        public DateTime? FaktisktStartLån { get; set; } //När boken faktiskt blev upphämtad av medlem, tex om upphämtning sker någon dag efter önskat startdatum på bokningen som angavs under menyval 1.
-        public DateTime? ÅterTid { get; set; } //När boken ska vara tillbaka lämnad, 14 dagar efter startdatum för bokningen
+        public DateTime StartLån { get; set; }  // Det datum som skrivs in när man först planerar att boka boken ifrån i menyval 1
+        public DateTime FaktisktStartLån { get; set; } //När boken faktiskt blev upphämtad av medlem, tex om upphämtning sker någon dag efter önskat startdatum på bokningen som angavs under menyval 1.
+        public DateTime ÅterTid { get; set; } //När boken ska vara tillbaka lämnad, 14 dagar efter startdatum för bokningen
         public bool UppHämtad { get; set; }
         public bool Återlämnad { get; set; }
 
